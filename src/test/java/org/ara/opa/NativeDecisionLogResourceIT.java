9c0d869d26be4f75917e88b9339a0e2db611c31a
Aravinth Alvapillai <aravinth.alvapillai@evry.com>
2020-09-15 19:31:25 +0200

    Project init

---
+++ src/test/java/org/ara/opa/NativeDecisionLogResourceIT.java
@@ -0,0 +1,9 @@
+package org.ara.opa;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDecisionLogResourceIT extends DecisionLogResourceTest {
+
+    // Execute the same tests but in native mode.
+}
